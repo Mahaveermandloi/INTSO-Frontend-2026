9475ddf59123a9f00c22aa38d950702aa3dc0447
mahaveer <mahaveermandloi9@gmail.com>
2026-02-07 22:46:16 +0530

    Resolved merge conflict

--- src/URLPath.jsx
+++ src/URLPath.jsx
@@ -1,6 +1,7 @@
 // export const URLPath = "https://intso-backend-vbs8.onrender.com";
-export const URLPath = "http://localhost:8000";
-// export const URLPath = "${PROD_BACKEND}";
+// export const URLPath = "http://localhost:8000";
+export const URLPath = "https://intso-backend-2026.onrender.com";
+// export const URLPath = "${PROD_BACKEND}";    
 
 // export const PROD_BACKEND = "http://localhost:8000";
 
